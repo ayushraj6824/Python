d5675cdebb10309e449be75aeecee3fb6d8475c7
Ayush kumar <ayush.17862@stu.upes.ac.in>
2024-08-21 23:24:44 +0530

    Added random.randint

--- Practice question/5.py
+++ Practice question/5.py
@@ -1,11 +1,8 @@
-size=10
+size=5
 alphabet=[chr(i) for i in range(97,123)]
-#print(alphabet)
 alphabet=alphabet[:size]
-#print(alphabet)
 l=list(range(size))
 l=l[:-1]+l[::-1]
-#print(l)
 for i in l:
     start=i+1
     origina=alphabet[-start:]
